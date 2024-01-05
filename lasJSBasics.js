5fae11455c74ec7d62d69b86b42d62e2a45cc8e0
creatifying121 <shivani.raichandani121@gmail.com>
2024-01-05 22:24:26 +0530

    commit

--- lasJSBasics.js
+++ lasJSBasics.js
@@ -3,10 +3,16 @@
 // syntax: function nameOfFunction(){ code to execute }
 // why we need functions? => reusability
 
-// FUNCTION DECLARATION
-// function functionName (input parameters (optional)) { body of function; }
-run();
-function run(){
-    console.log('running');
-}
-// function call/invocation
+// // FUNCTION DECLARATION
+// // function functionName (input parameters (optional)) { body of function; }
+// run();
+// function run(){
+//     console.log('running');
+// }
+// // function call/invocation
+
+
+// -------------- HOISTING --------------------
+// in javascript, it does not matter from where we are invoking a function. it gives the same result even calling the function after its definition and declaration or before it. 
+// this concept is hoisting
+// hoisting is process of moving function declaration to the top of the file, it is done by the JS Engine
