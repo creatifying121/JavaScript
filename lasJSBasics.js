7c4ffa5f5f476eb7ee9a86ec68ff0e63fb1394b2
creatifying121 <shivani.raichandani121@gmail.com>
2024-01-05 15:35:01 +0530

    commit

---
+++ lasJSBasics.js
@@ -0,0 +1,12 @@
+// --------------------- FUNCTIONS -----------------------
+// a block of code that fulfills a specific task is called function
+// syntax: function nameOfFunction(){ code to execute }
+// why we need functions? => reusability
+
+// FUNCTION DECLARATION
+// function functionName (input parameters (optional)) { body of function; }
+run();
+function run(){
+    console.log('running');
+}
+// function call/invocation
