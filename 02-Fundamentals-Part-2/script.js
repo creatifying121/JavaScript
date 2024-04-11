f2a677e60292109ba4c87b9fb6496b9d961bba82
creatifying121 <shivani.raichandani121@gmail.com>
2024-04-11 15:53:16 +0530

    commit

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -9,12 +9,42 @@
 // if (hasDriversLicense) console.log("you can drive");
 
 // Functions in JS
-function logger() {
-  console.log("I am shivani");
+// function logger() {
+//   console.log("I am shivani");
+// }
+
+// logger();
+// logger();
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `This is juice of ${apples} apples and ${oranges} oranges. Enjoy!`;
+
+//   return juice;
+// }
+
+// const res = fruitProcessor(3, 2);
+// console.log(res);
+
+// Function Declaration
+function calcAge(birthYear) {
+  return 2024 - birthYear;
 }
 
-logger();
-logger();
-logger();
-logger();
-logger();
+const age = calcAge(1997);
+console.log(age);
+
+// function expression
+const calcage2 = function (birthYear) {
+  return 2024 - birthYear;
+};
+
+console.log(calcage2(2000));
+
+// Arrow Function
+const calcAge3 = (birthYear) => 2024 - birthYear;
+
+console.log(calcAge3(2001));
