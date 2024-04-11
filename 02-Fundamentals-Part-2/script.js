7046fe5181cf9042b9ea1ac28d15315b8ab108f6
creatifying121 <shivani.raichandani121@gmail.com>
2024-04-11 12:38:54 +0530

    commit

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,20 @@
+// Activating strict mode
+"use strict";
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+
+// if (hasDriversLicense) console.log("you can drive");
+
+// Functions in JS
+function logger() {
+  console.log("I am shivani");
+}
+
+logger();
+logger();
+logger();
+logger();
+logger();
