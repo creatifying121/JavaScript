0158ce41c92df33bb5bab6e963d02ef392034f41
creatifying121 <shivani.raichandani121@gmail.com>
2024-04-16 01:13:51 +0530

    coding challenge objects commit

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -323,3 +323,104 @@
 // console.log(
 //   `${myData.firstName} has ${myData.friends.length}, and my best friend is ${myData.friends[2]}.`
 // );
+
+// Object Methods
+// const myData = {
+//   firstName: "Shivani",
+//   lastName: "Raichandani",
+//   birthYear: 2000,
+//   occupation: "React Developer",
+//   friends: ["Ayushi", "Purva", "Shivani(Me)"],
+//   hasDriversLicense: true,
+
+//   calcAge: function (birthYear) {
+//     return 2024 - birthYear;
+//   },
+// };
+
+// calling a function using dot and bracket notation
+// console.log(myData.calcAge(2000));
+// console.log(myData["calcAge"](2000));
+
+// but in above example we have to pass birthyear, even if this property is available inside the object itself.
+// so, to make it reusable, we can do something like this-
+// const myData = {
+//   firstName: "Shivani",
+//   lastName: "Raichandani",
+//   birthYear: 2000,
+//   occupation: "React Developer",
+//   friends: ["Ayushi", "Purva", "Shivani(Me)"],
+//   hasDriversLicense: true,
+
+//   // calcAge: function () {
+//   //   return 2024 - this.birthYear;
+//   // },
+
+//   calcAge: function () {
+//     this.age = 2024 - this.birthYear;
+//     return this.age;
+//   },
+
+//   // Challenge: "Shivani is a 24-year old React Developer and she has a/no (any one of them should be displayed) driver's license."
+//   getSummary: function () {
+//     return `${this.firstName} is a ${this.age}-year old ${
+//       this.occupation
+//     }, and she has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
+//   },
+// };
+
+// console.log(myData.calcAge());
+// console.log(myData.age);
+
+// console.log(myData.getSummary());
+
+// ============================== CODING CHALLENGE =================================
+// CHALLENGE #3
+// Let's go back to Mark and John comparing their BMIs!
+
+// This time, let's use objects to implement the calculations! Remember: BMI = mass / (height * height) (mass in kg and height in meters).
+
+// Your tasks:
+
+// For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith). Name these objects as mark and john, and their properties exactly as fullName, mass and height.
+
+// Create a calcBMI method on each object to calculate the BMI (the same method on both objects). Assign the BMI value to a property called bmi (lowercase), and also return it from the method.
+
+// Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!".
+
+// TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+/* Write your code below. Good luck! 🙂 */
+// const mark = {
+//   fullName: "Mark Miller",
+//   mass: 78,
+//   height: 1.69,
+
+//   calcBMI: function () {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   },
+// };
+
+// const john = {
+//   fullName: "John Smith",
+//   mass: 92,
+//   height: 1.95,
+
+//   calcBMI: function () {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   },
+// };
+
+// if (john.calcBMI() > mark.calcBMI()) {
+//   console.log(
+//     `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
+//   );
+// } else {
+//   console.log(
+//     `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
+//   );
+// }
+
+// =========================================================================
