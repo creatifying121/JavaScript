172337531ebe91a6dae9e8794fdc16d27d294965
creatifying121 <shivani.raichandani121@gmail.com>
2024-04-19 17:18:12 +0530

    coding challenge commit

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -101,3 +101,41 @@
 
 // // A) Identify
 // console.log(measureKelvin()); // prints 10273 for input 10. hence, bug identified, as expected result was 283
+
+// ============================= CODING CHALLENGE 1 ====================================
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+// 1. Understanding the problem
+// - Array should be converted to string in a special format
+// - a function should be written to do so
+
+// 2. Breaking into sub-problems
+// - write a function which takes array as parameter
+// - array should be converted to string
+// - array element should have ºC after it
+// - strings are separated using ...
+// - it needs to be printed on console
+
+const printForecast = function (arr) {
+  let str = "... ";
+
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  return str;
+};
+
+const testArray = [17, 21, 23];
+
+console.log(printForecast(testArray));
