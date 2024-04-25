6a69c3a253eb0731512acc7baccdf26658f72ced
creatifying121 <shivani.raichandani121@gmail.com>
2024-04-25 13:41:46 +0530

    gaming logic implemented commit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -19,6 +19,13 @@
 // document.querySelector(".guess").value = 12;
 // console.log(document.querySelector(".guess").value);
 
+// defining the secret number, we need to define it outside, because it should be defined only once, not more than that
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+// everytime a wrong guess is made, we need to decrease the score
+let score = 20;
+
 // now let's make our code do something for us and to achieve this we need event listeners
 // event is something that happens on a page, for example, mouse click, or a key press or many other events
 // we want to add an event listener on the check button, because we want that something should happen when we check the number we entered
@@ -29,5 +36,27 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too High!";
+
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "👎🏻 You lost!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too Low!";
+
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "👎🏻 You lost!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 }); // this function here is the event handler. this function will not be called immediately as the script is loaded, but it will only be called when the event (clicking on the check button) happens
