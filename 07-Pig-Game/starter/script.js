ecf0fb41e03f8ce0239ce6433cc54c2876b19a33
creatifying121 <shivani.raichandani121@gmail.com>
2024-05-01 12:12:29 +0530

    commit

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// selecting elements
+const score0El = document.querySelector('#score--0');
+// below is the another way of selecting elements using there id, it is more faster than the queryselector method
+const score1El = document.getElementById('score--1');
+
+const diceEl = document.querySelector('.dice');
+
+// starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+diceEl.classList.add('hidden');
