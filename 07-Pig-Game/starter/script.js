35001868accd5e0dced170af79a0fb136f083bc5
creatifying121 <shivani.raichandani121@gmail.com>
2024-05-01 18:51:15 +0530

    pig game complete

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -17,22 +17,42 @@ const current1El = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-// starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-
-diceEl.classList.add('hidden');
-
 // creating an array to save the scores of the players
-const scores = [0, 0];
+let scores;
 
-let currentScore = 0;
+let currentScore;
 
 // know who's the active player
-let activePlayer = 0;
+let activePlayer;
 
 // keeping track of game being played or not
-let playing = true;
+let playing;
+
+// initialization function
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+
+  // starting conditions
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+};
+
+init();
 
 // switching player function
 const switchPlayer = function () {
@@ -97,3 +117,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// resetting the game
+btnNew.addEventListener('click', init);
