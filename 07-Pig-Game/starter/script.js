3b2f4c8fffee1c0c058fe2e0c7eba1c09f7a2051
creatifying121 <shivani.raichandani121@gmail.com>
2024-05-01 17:26:10 +0530

    logic 2 commit

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -14,6 +14,9 @@ const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 // starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -41,8 +44,18 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // this needs to be changed later
+
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    // current0El.textContent = currentScore; // this needs to be changed later
   } else {
     // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
