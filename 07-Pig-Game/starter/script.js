983380842df03aa2c4ab49dcade53af2b28d55d7
creatifying121 <shivani.raichandani121@gmail.com>
2024-05-01 18:13:35 +0530

    logic 3 implemented

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -31,31 +31,69 @@ let currentScore = 0;
 // know who's the active player
 let activePlayer = 0;
 
+// keeping track of game being played or not
+let playing = true;
+
+// switching player function
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // implementing the rolling dice logic
 btnRoll.addEventListener('click', function () {
-  // generating a randome dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // generating a randome dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // display the dice roll
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    // display the dice roll
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // check if it is 1 , if so, switch the player
-  if (dice !== 1) {
-    // add dice to current score
-    currentScore += dice;
+    // check if it is 1 , if so, switch the player
+    if (dice !== 1) {
+      // add dice to current score
+      currentScore += dice;
 
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
 
-    // current0El.textContent = currentScore; // this needs to be changed later
-  } else {
-    // switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
+      // current0El.textContent = currentScore; // this needs to be changed later
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
+  }
+});
 
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+// holding the score
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. add current score to active player's score => scores[1] = scores[1]+currentScore
+    scores[activePlayer] += currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. check if player's score is >=100
+    // finish the game
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--active');
+
+      diceEl.classList.add('hidden');
+    } else {
+      // switch to the next player
+      switchPlayer();
+    }
   }
 });
