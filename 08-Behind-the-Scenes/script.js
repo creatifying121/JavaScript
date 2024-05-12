2565c29878f4499a2071ab910b7e34a68b005791
creatifying121 <shivani.raichandani121@gmail.com>
2024-05-13 05:20:24 +0530

    commit - this keyword practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -230,3 +230,47 @@ jessicaCopy.family.push('John');
 console.log('Before marriage:', jessica2);
 console.log('After marriage: ', jessicaCopy);
 */
+
+// =========================================================================
+
+// this keyword - my practice
+console.log(this); // it refers to the window object, hence o/p is window object
+
+const calcAge = function (birthYear) {
+  console.log(2024 - birthYear);
+  console.log(this); // undefined (in regular function call, this keyword is undefined)
+};
+calcAge(2000); // since we are in strict mode, it will give undefined, if we were in sloppy mode, then it would have been the global object
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2024 - birthYear);
+  console.log(this); // window (arrow function does not get its own this function, it uses its lexical parent to find the value of this, and here its lexical parent is the global scope, thus, window would be printed on the console)
+};
+calcAgeArrow(2001);
+
+const shivani = {
+  year: 2000,
+  calcAge: function () {
+    console.log(this); // shivani object data (it refers to shivani object, because when we have a method call - [function related to an object is called] then the this keyword refers to the object itself from where function is called)
+    console.log(2024 - this.year);
+  },
+};
+shivani.calcAge();
+
+// we know that this keyword point to the object that is calling the method. so this means that this keyword will not simply point at the object in which we wrote the method.
+
+// let's say, above we wrote calcAge() method in shivani object, so there can be a confusion that this keyword might be pointing to shivani, because it is written inside shivani object. but that's not true.
+
+// the reason that the this keyword will point to shivani , because shivani is the object calling that method. that's a subtle but very important difference.
+const purva = {
+  year: 1999,
+};
+purva.calcAge = shivani.calcAge; // copying the calcage method from shivani object to purva object - this is called method borrowing
+console.log(purva);
+purva.calcAge(); // 25 (this keyword always points to the object that is calling the method. even if it is written inside another object)
+
+// now what if we move the function completely out.
+const f = jonas.calcAge();
+f(); // here this keyword is undefined, we will also get an error "cannot read property 'year' of undefined, because this.year means undefined.year, so value cannot be calculated"
+
+// this is because, f function is now just a regular function call, it is not attached to any object. basically no one owns the f function. so in that case, this keyword will be undefined.
