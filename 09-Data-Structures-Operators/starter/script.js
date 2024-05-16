524459c5bd8c168195728232fa070ef33e22e78a
creatifying121 <shivani.raichandani121@gmail.com>
2024-05-16 15:20:29 +0530

    OBJECT DESTRUCTURING COMMIT

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -16,21 +16,38 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  // openingHours: {
-  //   thu: {
-  //     open: 12,
-  //     close: 22,
-  //   },
-  //   fri: {
-  //     open: 11,
-  //     close: 23,
-  //   },
-  //   sat: {
-  //     open: 0, // Open 24 hours
-  //     close: 24,
-  //   },
-  // },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  // practical application of object destructuring
+  // orderDelivery: function (obj) {
+  // it is common, because now here in the function arguments , we can actually do destructuring right away
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    // see here order of arguments does not matters. default values can also be set here
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
+
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Bhopal",
+  mainIndex: 2,
+  starterIndex: 2,
+}); // this is pretty common when we use APIs or any third party websites to fetch data
 
 // =============================================== DESTRUCTURING ARRAY START=============================
 /*
@@ -94,3 +111,61 @@ const [m = 1, n = 1, o = 1] = [8, 9];
 console.log(m, n, o); // 8 9 1 => in this way, where array element does not exist , the variable will hold the default value assigned to it.
 */
 // =============================================== DESTRUCTURING ARRAY END =============================
+
+// =============================================== DESTRUCTURING OBJECT START =============================
+/*
+// while destructuring objects, we have to use the exact same properties name as we have used while declaring the object. and because of this, the order does not matter
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// this is extremely imp when we deal with the result of API calls. this data comes in form of objects, and thus destructuring is very imp
+
+// now what if we want that variable names should be different from the property names?
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// setting default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters); // menu will now give an empty array while if the default value wasn't assigned to it, then it would have provided undefined as an output, as menu is not there in the restaurant object
+
+// mutating variables while destructuring objects
+// in arrays we used swapping method and then we used another method of reassigning the values to each other
+let a = 111;
+let b = 999;
+// we want to destructure below object obj
+const obj = {
+  a: 23,
+  b: 7,
+  c: 14,
+};
+
+// let's try doing mutation
+// {a,b} = obj; // this throws an error "Unexpected token '='" : this is because, whenever we write curly braces, JS expects a code block there, and we cannot assign anything to a code block like in this example we did using equal sign, thus error was there
+
+// so here the twist is that we need to combine this all in parenthesis
+({ a, b } = obj);
+console.log(a, b);
+
+// nested objects
+// const { fri } = openingHours;
+// console.log(fri); // this will give the fri object as a whole
+
+// but we want open and close hours as variables
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// we can provide custom variable names to these too
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c); // 11 23
+
+// practical application of object destructuring
+
+// many times in javascript we have functions with a lot of parameters, but then it can be hard to know the order of the parameters for someone that is using this function. And so instead of defining the parameters manually, we can just pass an object into the function as an argument, and then function will immediately destructure that object.
+*/
+// =============================================== DESTRUCTURING OBJECT END =============================
