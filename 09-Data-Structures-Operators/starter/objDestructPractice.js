dcbbc09fad799aac09335d1a6d68bc58b14e0d5f
creatifying121 <shivani.raichandani121@gmail.com>
2024-05-20 13:12:07 +0530

    object destructuring practice done commit.

--- 09-Data-Structures-Operators/starter/objDestructPractice.js
+++ 09-Data-Structures-Operators/starter/objDestructPractice.js
@@ -243,3 +243,42 @@ let bookAuthor = "unknown";
 
 ({ title: bookTitle, author: bookAuthor } = books[0]);
 console.log(bookTitle, bookAuthor);
+
+// 2.5 Each book object has a deeply nested rating property as illustrated below:
+// {
+//   title: 'Algorithms',
+//   ...
+//   thirdParty: {
+//     goodreads: {
+//       rating: 4.41,              // <-- HERE
+//       ratingsCount: 1733,
+//       reviewsCount: 63,
+//       fiveStarRatingCount: 976,
+//       oneStarRatingCount: 13
+//     }
+//   }
+// },
+
+// Destructure the first book object from the books array into a variable called bookRating. In the result of your destructuring, the bookRating variable should be assigned with the value of the book[0].thirdParty.goodreads.rating property.
+
+// Please do most of the work on the left side of the assignment operator: const ... = books[0];
+const {
+  thirdParty: {
+    goodreads: { rating: bookRating },
+  },
+} = books[0];
+console.log(bookRating);
+
+// 2.6 Write a function called printBookInfo that has three parameters called title, author and year. This function should work for a single object passed as an argument, and it should log to the console information about the book in this format: "${title} by ${author}, ${year}".
+
+// If year is undefined (was not passed), it should be assigned with a default value of 'year unknown'.
+function printBookInfo({ title, author, year = "year unknown" }) {
+  console.log(`${title} by ${author} in ${year}`);
+}
+
+printBookInfo({
+  title: "Algorithms",
+  author: "Robert Sedgewick",
+  year: "2011",
+});
+printBookInfo({ title: "Frontend", author: "Shivani Raichandani" });
