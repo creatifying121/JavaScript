c9930f75c110a87ee5ac5473b267e87fc87e236f
Shivani Raichandani <132776785+creatifying121@users.noreply.github.com>
2024-08-06 18:11:25 +0530

    commit

---
+++ main/01-basics/01-variables.js
@@ -0,0 +1,8 @@
+const accountId = 210300;
+let accountEmail = "shivani@meesho.com";
+var accountPassword = "shiv@123";
+accountCity = "bhopal";
+
+// accountID = 2130 {cannot be changed , not allowed}
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ main/01-basics/02-datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; // always use new version of js
+
+// to use alert in node, we have a different syntax don't use below while working with nodejs + javascript
+// alert("hello");
+
+// this alert('hello') is valid while running js code in browser console or while running js code by binding it with html code, as we know that all the js works in browser using documents, and that's not the case with node
