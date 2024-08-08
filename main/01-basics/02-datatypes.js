a730942306c27c3ac13e69249ee6fc5427118608
Shivani Raichandani <132776785+creatifying121@users.noreply.github.com>
2024-08-08 06:58:50 +0530

    commit datatypes

--- main/01-basics/02-datatypes.js
+++ main/01-basics/02-datatypes.js
@@ -4,3 +4,28 @@
 // alert("hello");
 
 // this alert('hello') is valid while running js code in browser console or while running js code by binding it with html code, as we know that all the js works in browser using documents, and that's not the case with node
+
+/*
+  different data types available
+
+  (Primitive data types)
+    - number
+    - string
+    - bigint
+    - boolean
+    - null (standalone value) - represents empty value 
+    - undefined - represents the value which is declared but not defined yet
+    - symbol - mostly used while working with react, when we cannot find out which component is unique, so symbol is used to find out which component is unique.
+
+    (Reference data types)
+    - objects
+*/
+
+/*
+  typeof
+
+  console.log(typeof "shivani")
+*/
+
+console.log(typeof null); // => it is sometimes called as error of language {output: object}
+console.log(typeof undefined); // => {output: undefined}
