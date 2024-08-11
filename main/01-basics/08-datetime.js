2b5cdf27e20ac49b5a87bfa3970810c2a545227e
Shivani Raichandani <132776785+creatifying121@users.noreply.github.com>
2024-08-11 07:35:57 +0530

    commit dates

---
+++ main/01-basics/08-datetime.js
@@ -0,0 +1,27 @@
+// dates
+let d = new Date();
+// console.log(d); // output {2024-08-09T14:37:37.863Z}
+
+// console.log(d.toString()); // output {Fri Aug 09 2024 20:07:37 GMT+0530 (India Standard Time)}
+
+// console.log(d.toISOString()); // output {2024-08-09T14:38:33.450Z}
+
+// console.log(d.toDateString()); // output {Fri Aug 09 2024}
+
+// console.log(d.toJSON()); // output {2024-08-09T14:47:44.706Z}
+
+// console.log(d.toLocaleDateString()); // output {8/9/2024}
+
+// console.log(d.toLocaleString()); // output {8/9/2024, 8:18:52 PM}
+
+// console.log(d.toLocaleTimeString()); // output {8:18:52 PM}
+
+// console.log(d.toTimeString()); // output {20:19:40 GMT+0530 (India Standard Time)}
+
+// console.log(typeof d); // it is an object hence output { object}
+
+// let myCreatedDate = new Date(2024, 0, 1); // 24 is year {if 24 is specified, it will take 1924 by default}, 0 is month (january) and 1 is date
+// console.log(myCreatedDate.toDateString()); // {output: Tue Jan 01 1924}
+
+// let myCreatedDate = new Date(2024, 0, 1, 12, 32); // here 12, 32 depict the time 12:32:00 PM
+// console.log(myCreatedDate.toLocaleString());
