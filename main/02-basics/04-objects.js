c2c59567c56121afe2e83ed41dbc466b53ff75dc
Shivani Raichandani <132776785+creatifying121@users.noreply.github.com>
2024-08-13 12:20:54 +0530

    commit

---
+++ main/02-basics/04-objects.js
@@ -0,0 +1,19 @@
+const hikeUser = {};
+hikeUser.id = "21300shivi";
+hikeUser.name = "Shivani";
+hikeUser.isLoggedIn = false;
+
+// console.log(hikeUser);
+
+// nested objects
+const user = {
+  email: "shivani@meesho.com",
+  fullname: {
+    userFullName: {
+      firstName: "Shivani",
+      lastName: "Raichandani",
+    },
+  },
+};
+
+// accessing the nested object's valuez
