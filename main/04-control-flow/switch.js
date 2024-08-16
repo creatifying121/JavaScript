5d2a56b83ee09a2c80aa4c9d15fbd4f1bc5b36b1
Shivani Raichandani <132776785+creatifying121@users.noreply.github.com>
2024-08-16 20:11:35 +0530

    switch case commit

---
+++ main/04-control-flow/switch.js
@@ -0,0 +1,76 @@
+// BASIC SYNTAX
+// switch (key) {
+//   case value:
+
+//     break;
+
+//   default:
+//     break;
+// }
+
+// LET'S TAKE AN EXAMPLE
+const month = 3;
+switch (month) {
+  case 1:
+    console.log("january");
+    break;
+  case 2:
+    console.log("february");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+  case 5:
+    console.log("may");
+    break;
+  case 6:
+    console.log("june");
+    break;
+  case 7:
+    console.log("july");
+    break;
+  case 8:
+    console.log("august");
+    break;
+  case 9:
+    console.log("september");
+    break;
+  case 10:
+    console.log("october");
+    break;
+  case 11:
+    console.log("november");
+    break;
+  case 12:
+    console.log("december");
+    break;
+
+  default:
+    console.log("wrong value");
+
+    break;
+}
+
+// same is with the string values
+const months = "march";
+switch (months) {
+  case "march":
+    console.log("teesra mahina");
+    break;
+  case "jan":
+    console.log("pehla mahina");
+    break;
+  case "feb":
+    console.log("dusra mahina");
+    break;
+
+  default:
+    console.log("sahi btao bhyii");
+
+    break;
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
