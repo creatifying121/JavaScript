afb29f030a9b870343c16404ff1a254e35867535
Shivani Raichandani <132776785+creatifying121@users.noreply.github.com>
2024-08-20 23:31:59 +0530

    commit

---
+++ main/05-iterations/array-loops.js
@@ -0,0 +1,76 @@
+// for of loop => for array
+let arr = [1, 2, 3, 4, 5];
+for (const i of arr) {
+  // console.log(i); // {output: 1 2 3 4 5}
+}
+
+// for of loop => for string
+let str = "hello there";
+for (const s of str) {
+  // console.log(s); // {output: h e l l o  t h e r e => everything in a new line is printed}
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// loops on map
+const map = new Map();
+// map.set("1", "tea");
+// map.set("1", "kokam sharbat");
+// map.set("1", "orange juice");
+// map.set("1", "coffee");
+
+// console.log(map); // Map(1) { '1' => 'coffee' } => when keys are same, then it takes the last key value pair as element in map
+
+map.set("1", "tea");
+map.set("2", "kokam sharbat");
+map.set("3", "orange juice");
+map.set("4", "coffee");
+
+// console.log(map); // Map(4) {
+//   '1' => 'tea',
+//   '2' => 'kokam sharbat',
+//   '3' => 'orange juice',
+//   '4' => 'coffee'
+// }
+
+map.set("2", "kokam sharbat"); // duplicates not allowed in map , also maintains the order in which elements are entered
+// console.log(map); // Map(4) {
+//   '1' => 'tea',
+//   '2' => 'kokam sharbat',
+//   '3' => 'orange juice',
+//   '4' => 'coffee'
+// }
+
+// loop on map
+
+// using for of loop and array destructuring we can access elements of map
+
+// 1st example
+for (const key of map) {
+  // console.log(key);
+}
+// output
+// [ '1', 'tea' ]
+// [ '2', 'kokam sharbat' ]
+// [ '3', 'orange juice' ]
+// [ '4', 'coffee' ]
+
+// 2nd example (using destructuring, we can get the desired output)
+for (const [key, value] of map) {
+  // console.log(key + ": " + value);
+}
+//output
+// 1: tea
+// 2: kokam sharbat
+// 3: orange juice
+// 4: coffee
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// FOR OF LOOP IN OBJECT
+const obj = {
+  1: "tea",
+  2: "kokam sharbat",
+  3: "orange juice",
+  4: "coffee",
+};
